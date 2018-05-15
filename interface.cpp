ccdd939b8e054ac389e929b9083a262789e32fc0
Markovvn1 <32509100+Markovvn1@users.noreply.github.com>
2018-05-15 16:01:52 +0700

    Create interface.cpp

---
+++ interface.cpp
@@ -0,0 +1,66 @@
+#include "interface.hpp"
+
+Interface::Interface()
+{
+	count = new int;
+	*count = 1;
+
+	lockData = new mutex;
+};
+
+Interface::Interface(const Interface &p)
+{
+	count = p.count;
+	lockData = p.lockData;
+
+	lockData->lock();
+	(*count)++;
+	lockData->unlock();
+}
+
+Interface::~Interface()
+{
+	destroy();
+}
+
+void Interface::distructor() {};
+
+void Interface::destroy()
+{
+	if (!active) return;
+	active = false;
+
+	int currentCount;
+
+
+	lockData->lock();
+
+	(*count)--;
+	currentCount = *count;
+
+	lockData->unlock();
+
+	if (currentCount < 0) cout << "Fatal error! count < 0" << endl;
+
+	if (currentCount <= 0)
+	{
+		distructor();
+
+		delete lockData;
+		delete count;
+	}
+}
+
+void Interface::operator =(const Interface &p)
+{
+	p.lockData->lock();
+	(*p.count)++;
+	p.lockData->unlock();
+
+	destroy();
+
+	lockData = p.lockData;
+
+	count = p.count;
+	active = true;
+}
